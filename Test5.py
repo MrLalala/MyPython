8f5400d08cbe4ad179f91ee15bc82c6be0fe3b36
Gan <1354410847@qq.com>
2017-03-20 22:59:18 +0800

    正确的做法

--- Test5.py
+++ Test5.py
@@ -1,16 +1,22 @@
 #coding:utf-8
 #判断三个整数的大小,并输出
-List = []
-List.append(int(raw_input("第一个数")))
-List.append(int(raw_input("第二个数")))
-List.append(int(raw_input("第三个数")))
-
-for x in range(0,2):
-    for y in range(1,3-x):
-        if List[y]<List[y-1]:
-            temp = List[y]
-            List[y] = List[y-1]
-            List[y-1]=temp
-print List
-
+# List = []
+# List.append(int(raw_input("第一个数")))
+# List.append(int(raw_input("第二个数")))
+# List.append(int(raw_input("第三个数")))
+#
+# for x in range(0,2):
+#     for y in range(1,3-x):
+#         if List[y]<List[y-1]:
+#             temp = List[y]
+#             List[y] = List[y-1]
+#             List[y-1]=temp
+# print List
+l = []
+for i in range(3):
+    x = int(raw_input('integer:\n'))
+    l.append(x)
+l.sort()
+print l
+#正确的和高效的
 #感觉没必要用冒泡排序
