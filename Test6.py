e92c65180a68d587510be9fcfa81d8f201f14745
Gan <1354410847@qq.com>
2017-03-21 10:08:35 +0800

    Test6 done

--- Test6.py
+++ Test6.py
@@ -1,2 +1,26 @@
 #coding:utf-8
 
+#非nab序列
+List = []
+# def fn(i):
+#     fn(0) = 1
+#python的递归函数：
+def fn(i):
+    if i ==0 or i ==1:
+        return 1
+    return fn(i-1)+fn(i-2)
+
+for i in range(10):
+    List.append(fn(i))
+print List
+#访法2：
+def fn2(i):
+    a,b = 1,1
+    for i in range(i-1):
+        a,b = b,a+b
+    return b
+
+for i in range(10):
+    List.append(fn(i))
+print List
+
